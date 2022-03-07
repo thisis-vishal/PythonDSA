4874177a1b8e4b7193f555cbdc50322d88158f39
Vishal Kumar <76644828+thisis-vishal@users.noreply.github.com>
2022-03-07 14:33:04 +0530

    Create searching_algorithm.py

---
+++ searching_algorithm.py
@@ -0,0 +1,40 @@
+# Searching algorithms - Linear Search
+
+def linearSearch(array, value):
+    for i in range(len(array)):
+        if array[i] == value:
+            return i
+    return -1
+
+
+
+print(linearSearch([20,40,30,50,90], 90))
+
+# Searching algorithms - Binary Search
+import math
+def binarySearch(array, value):
+    start = 0
+    end = len(array)-1
+    middle = math.floor((start+end)/2)
+    while not(array[middle]==value) and start<=end:
+        if value < array[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1 
+        middle = math.floor((start+end)/2)
+        # print(start, middle, end)
+    if array[middle] == value:
+        return middle
+    else:
+        return -1
+        
+
+
+
+custArray = [8, 9, 12, 15, 17, 19, 20, 21, 28]
+print(binarySearch(custArray, 15))
+# [8, 9, 12, 15, 17, 19, 20, 21, 28]
+#  S              M               E
+#  S  M      E
+#        SM  E
+#            SME
